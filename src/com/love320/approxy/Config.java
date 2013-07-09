110d32837b9e0be10a19dc60d16cca1a17d04bdc
Love320 <admin@love320.com>
2013-07-09 21:32:55 +0800

    （添加）配置修改为配置文件管理方式
    Signed-off-by: Love320 <admin@love320.com>

--- src/com/love320/approxy/Config.java
+++ src/com/love320/approxy/Config.java
@@ -37,19 +37,43 @@ public class Config {
 	static{
 		
 		//.逆向突破式代理
-		IPORTLIST.add(IPort.reverseProxy(6522,"127.0.0.1",22)); //6522 -> 127.0.0.1:22
+		//IPORTLIST.add(IPort.reverseProxy(6522,"127.0.0.1",22)); //6522 -> 127.0.0.1:22
 		
 		//普通代理
-		IPORTLIST.add(IPort.proxy(6699, "113.243.142.23", 9999));
+		//IPORTLIST.add(IPort.proxy(6699, "113.243.142.23", 9999));
 		
+		
+		/** 配置文件管理 */
 		Properties props = new Properties();
 		Set<String> keys = null;
 		try {
 			InputStream in = new FileInputStream(Config.CONF);
 			props.load(in);
 			keys = props.stringPropertyNames();
+			
+			if(props.getProperty("PROXY_HOST") != null ) PROXY_HOST = props.getProperty("PROXY_HOST");
+			if(props.getProperty("PROXY_TO_DOC") != null ) PROXY_TO_DOC = Integer.parseInt(props.getProperty("PROXY_TO_DOC"));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		
+		if(keys != null){
+			for(String key:keys){
+				String value = props.getProperty(key);
+				String ss[] = value.split("@");
+				if(ss.length == 2){
+					String ips[] = ss[1].split(":");
+					if(key.toUpperCase().indexOf("A") ==0){
+						//普通代理
+						IPORTLIST.add(IPort.proxy(Integer.parseInt(ss[0]), ips[0], Integer.parseInt(ips[1])));
+					}
+					if(key.toUpperCase().indexOf("B") ==0){
+						//.逆向突破式代理
+						IPORTLIST.add(IPort.reverseProxy(Integer.parseInt(ss[0]), ips[0], Integer.parseInt(ips[1])));
+					}
+				}
+			}
 		}
 
 		
